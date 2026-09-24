5222df7781231901eb7977ad41c27e875abed7aa
D Gunasekhar <dandugunasekhar7093@gmail.com>
2026-09-25 02:13:36 +0530

    Make offline user detection near-instant

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
-    pingInterval: 5000,
-    pingTimeout: 10000
+    pingInterval: 1000,
+    pingTimeout: 1000
 });
 
 app.use(express.static("public"));
